2924d09cdff4af2ffa8b69529fd4a9ad334ae3c5
sanqi777 <50070756+sanqi777@users.noreply.github.com>
2020-12-21 20:05:49 +0800

    Java源码测试分析

---
+++ src/com/san/test/HsahMap.java
@@ -0,0 +1,17 @@
+package com.san.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HsahMap {
+    public static void main(String[] args) {
+        Map<String,Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        map.put("k1",1);
+        map.put("k2",2);
+        map.put("k3",3);
+        map.put("k4",4);
+    }
+}
